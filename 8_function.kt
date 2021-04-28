ffc9a0588da1ef2c0c253650f8d6bf90fc047550
Jithendiran <jidesh0410@gmail.com>
2021-04-28 13:56:37 +0530

    default_function

--- 8_function.kt
+++ 8_function.kt
@@ -1,3 +1,12 @@
+// java can call function in kotlin
+// kotlin can call function in java
+// any thing can possible class 
+
+//from java call 8_functionKt.add(1,2) like that because it is not in class it is static like behaviour or below step
+
+//@file:JvmName("anyname") this line must in kotlin file and in java call like this anyname.add(1,2)
+
+
 fun add(i:Int, j:Int):Int
 {
     return i+j
@@ -7,10 +16,16 @@ fun sub(i:Int, j:Int):Int=i-j
 
 fun max(i:Int, j:Int):Int= if(i<j) j else i
 
+//default argument
+fun def(a:Int, b:Int=20):Int=a*b
+
 
 fun main() 
 {
     println(add(1,2))
     println(sub(6,3))
     println(max(9,8))
+    println(def(1))
+    println(def(2,3))
+
 }
